7b6df83724395898d05e08e3fff32d89678a0f8b
Nitaliss <megustamucholatecno@gmail.com>
2026-05-11 16:44:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // © 2026 Alicia Prats · tapunto.app
 // IMPORTANTE: cambia el número de CACHE_NAME cada vez que subas una versión nueva
 
-const CACHE_NAME =  'tapunto-v3.4';
+const CACHE_NAME =  'tapunto-v4.7';
 const ASSETS = [
   '/',
   '/index.html',
